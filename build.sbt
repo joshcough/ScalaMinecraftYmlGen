3e5fa50f162db9acec80fd8f6907e9e56d0f002b
joshcough <joshcough@gmail.com>
2013-08-05 23:10:32 -0400

    officially releasing 0.3.1

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-organization := "jcdc.pluginfactory"
+organization := "com.joshcough"
 
 name := "scala-minecraft-yml-gen"
 
@@ -12,14 +12,12 @@ sbtVersion := "0.13.0-RC4"
 
 scalaVersion := "2.10.2"
 
-resolvers += "joshcough bintray maven" at "http://dl.bintray.com/joshcough/maven/"
-
-libraryDependencies += "jcdc.pluginfactory" %% "scala-minecraft-plugin-api" % "0.3.1"
+libraryDependencies += "com.joshcough" %% "scala-minecraft-plugin-api" % "0.3.1"
 
 scalacOptions ++= Seq(Opts.compile.deprecation)
 
 licenses <++= (version)(v => Seq("MIT" -> url(
-  "https://github.com/joshcough/scala-minecraft-yml-gen/blob/%s/LICENSE".format(v))))
+  "https://github.com/joshcough/ScalaMinecraftYmlGen/blob/%s/LICENSE".format(v))))
 
 publishMavenStyle := true
 
--- src/main/scala/plugin.scala
+++ src/main/scala/plugin.scala
@@ -1,22 +1,21 @@
-package com.joshcough
+package com.joshcough.minecraft
 
 import java.io.File
 import sbt._
 import Keys._
-import jcdc.pluginfactory.YMLGenerator._
+import com.joshcough.minecraft.YMLGenerator._
 
 object Plugin extends sbt.Plugin {
 
-  def pluginYmlSettings(author: String): Seq[Setting[_]] = Seq[Setting[_]](
+  def pluginYmlSettings(pluginClassname: String, author: String): Seq[Setting[_]] = Seq[Setting[_]](
     resourceGenerators in Compile <+=
-      (resourceManaged in Compile, streams, name, productDirectories in Compile, dependencyClasspath in Compile, version, compile in Compile, runner) map {
-        (dir, s, name, cp1, cp2, v, _, r) =>
+      (resourceManaged in Compile, streams, productDirectories in Compile, dependencyClasspath in Compile, version, compile in Compile, runner) map {
+        (dir, s, cp1, cp2, v, _, r) =>
           Run.run(
-            "jcdc.pluginfactory.YMLGenerator", (Attributed.blankSeq(cp1) ++ cp2).map(_.data),
-            Seq("jcdc.pluginfactory.examples." + name, author, v, dir.getAbsolutePath),
+            "com.joshcough.minecraft.YMLGenerator", (Attributed.blankSeq(cp1) ++ cp2).map(_.data),
+            Seq(pluginClassname, author, v, dir.getAbsolutePath),
             s.log)(r)
           Seq(dir / "plugin.yml", dir / "config.yml")
       }
   )
-
 }
